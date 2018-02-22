3ffa4f6ef8108742a6d8ecce3610f5ffdbee343a
Nisheed Jagadish 2 <njaga2@sapient.com>
2018-02-22 17:41:27 +0530

    Added fractulus basic docs

---
+++ docs/fractulus/_fractulus/scripts/theme.js
@@ -0,0 +1,10 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(factory());
+}(this, (function () { 'use strict';
+
+/** ES5 Theme JS if any goes here. */
+console.log("Theme JS initialized!!!");
+
+})));
---
+++ docs/fractulus/scripts/app.js
@@ -0,0 +1,10 @@
+this.DNG = this.DNG || {};
+this.DNG.App = (function (stimulus) {
+'use strict';
+
+var app = stimulus.Application.start();
+
+return app;
+
+}(Stimulus));
+//# sourceMappingURL=app.js.map
